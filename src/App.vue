812bdffe15d41694825d8b78ff91c008d1973e1c
pamecorreabarrera@gmail.com <pamecorreabarrera@gmail.com>
2023-06-15 10:57:51 -0400

    Commit 3: Se aplica lazy_loading y animaciones a las vistas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <navigation/>
-    <router-view/>
+    <!-- Se aplica la etiqueta "transition" a "router-view" para aplicarle transiciones a la vista -->
+    <!-- mode="out-in": la vista que está renderizada la saca y luego entra la nueva vista  -->
+    <!-- name="router-anim" : nombre de la animación , nombre de la clase que se utilizará en css -->
+    <transition name="router-anim" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 <script>
@@ -28,5 +33,40 @@ export default {
     body{
   background-image: url("https://i.pinimg.com/originals/a4/5e/e4/a45ee418aa77783d1a847190596d9796.jpg");
   background-size: cover;
+}
+/* clase ligada a la acción "enter-active": cuando entra el elemento*/
+.router-anim-enter-active{
+  /* Implementación de la animación "coming" junto con el tiempo que demora */
+  animation: coming .5s;
+  animation-delay: .5s;
+  opacity: 0;
+}
+/* clase ligada a la acción "leave-active":cuando se va el elemento */
+.router-anim-leave-active{
+  animation: going .7s;
+}
+/* Animación coming */
+@keyframes coming{
+  /* from: en el 0% de la duración de la animación */
+  from{
+    transform: translateX(-50px);
+    opacity: 0;
+  }
+  /* from: en el 100% de la duración de la animación */
+  to{
+    transform: translateX(0px);
+    opacity: 1;
+  }
+}
+/* Animación going */
+@keyframes going{
+  /* 0% y 100% es lo mismo que el from to, pero se puede dividir en más etapas de la animación */
+  0%{
+    transform: translateX(0px);
+  }
+  100%{
+    transform: translateX(-50px);
+    opacity: 0;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Inicio from '@/views/Inicio.vue'
-import Personajes from '@/views/Personajes.vue'
-import Personaje from '@/views/Personaje.vue'
-import Contacto from '@/views/Contacto.vue'
+// import Inicio from '@/views/Inicio.vue'
+// import Personajes from '@/views/Personajes.vue'
+// import Personaje from '@/views/Personaje.vue'
+// import Contacto from '@/views/Contacto.vue'
 import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
@@ -12,7 +12,8 @@ const routes = [
   {
     path: '/',
     name: 'inicio',
-    component: Inicio
+    //Se aplica "lazy loading" para que las rutas carguen por separado y la página cargue rápidamente la primera vez
+    component: () => import(/* webpackChunkName: "inicio" */ '@/views/Inicio.vue')
   },
   {
     path: '/personajes',
@@ -20,20 +21,23 @@ const routes = [
     /* Se crean 2 alias para el path "personajes" de manera que si el usuario ingresa a "people"
      o "characters", se redirija a la vista de "personajes" */
     alias: ['/people', '/characters'],
-    component: Personajes
+    //Se aplica "lazy loading" para que las rutas carguen por separado y la página cargue rápidamente la primera vez
+    component: () => import(/* webpackChunkName: "personajes" */ '@/views/Personajes.vue')
   },
   {
     //Path dinámico.
     path: '/personajes/:id',
     name: 'personaje',
-    component: Personaje,
+    //Se aplica "lazy loading" para que las rutas carguen por separado y la página cargue rápidamente la primera vez
+    component: () => import(/* webpackChunkName: "personaje" */ '@/views/Personaje.vue'),
     //El componente "Personaje" recibirá el id por una prop, por lo que hay que habilitarlo poniendo true
     props: true
   },
   {
     path: '/contacto',
     name: 'contacto',
-    component: Contacto
+    //Se aplica "lazy loading" para que las rutas carguen por separado y la página cargue rápidamente la primera vez
+    component: () => import(/* webpackChunkName: "contacto" */ '@/views/Contacto.vue')
   },
   {
     //Router comodín
